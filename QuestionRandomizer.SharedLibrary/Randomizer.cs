90376bdf45f19dc3b01c2f6bced1c093b51a88b2
harbargerdev <aharbarger@gmail.com>
2020-01-08 17:45:35 -0500

    Some base implementation for setting and randomizing

--- QuestionRandomizer.SharedLibrary/Randomizer.cs
+++ QuestionRandomizer.SharedLibrary/Randomizer.cs
@@ -1,17 +1,23 @@
-﻿using System;
+using System;
 using QuestionRandomizer.SharedLibrary.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace QuestionRandomizer.SharedLibrary
 {
     public class Randomizer : IRandomizer
     {
-        private readonly List<Question> _initialQuestions;
+        private List<Question> _initialQuestions;
         private List<Question> _availableQuestions;
+
+        public Randomizer()
+        {
+        }
 
         public Randomizer(string jsonQuestions)
         {
+            ConvertJsonToList(jsonQuestions);
         }
 
         public Randomizer(ICollection<Question> questions)
@@ -19,9 +25,39 @@ namespace QuestionRandomizer.SharedLibrary
             _initialQuestions = questions.ToList();
         }
 
-        public ICollection<Question> RandomizeQuestions()
+        public List<Question> RandomizeQuestions(int count)
         {
-            throw new System.NotImplementedException();
+            if (count > _initialQuestions.Count)
+                throw new Exception("Invalid parameter sent, count should be less than initial questions.");
+            else if (count == _initialQuestions.Count)
+                return _initialQuestions; // Shortcut if full list is requested
+            
+            List<Question> questions = new List<Question>();
+
+
+
+            return questions;
         }
+
+        public bool SetInitialQuestions(string jsonQuestions)
+        {
+            ConvertJsonToList(jsonQuestions);
+            return true;
+        }
+
+        public bool SetInitialQuestions(ICollection<Question> questions)
+        {
+            _initialQuestions = questions.ToList();
+            return true;
+        }
+
+        #region Helper Methods
+
+        private void ConvertJsonToList(string json)
+        {
+            _initialQuestions = JsonConvert.DeserializeObject<List<Question>>(json);
+        }
+
+        #endregion
     }
 }
